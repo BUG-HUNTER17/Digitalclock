e439067145da4b558e7301543d15fe583e8ded6f
Arnab banerjee <101695707+BUG-HUNTER17@users.noreply.github.com>
2023-12-16 23:23:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+var timerID = setInterval(updateTime, 1000);
+updateTime();
+
+function updateTime() {
+  var cd = new Date();
+  var currentTime = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+  var currentDate = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+
+  document.getElementById('currentTime').innerHTML = currentTime;
+  document.getElementById('currentDate').innerHTML = currentDate;
+}
+
+function zeroPadding(num, digit) {
+  var zero = '';
+  for(var i = 0; i < digit; i++) {
+      zero += '0';
+  }
+  return (zero + num).slice(-digit);
+}
